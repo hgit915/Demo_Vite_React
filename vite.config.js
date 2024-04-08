435289a30e8bbbfbe07d362e42d14960322fef39
qianyi <hgit915@gmail.com>
2024-04-08 17:31:09 +0800

    config : alias / css setting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+const resolve = (pathname) => path.resolve(__dirname, pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/Demo_Vite_React/" : "/",
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve("src"),
+      "~": resolve(""),
+      components: resolve("src/components"),
+    },
+  },
 });
