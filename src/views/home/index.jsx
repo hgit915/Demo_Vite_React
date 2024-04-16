ecdc00aaff1fea1a305f127d4713fda8bffd3200
qianyi <hgit915@gmail.com>
2024-04-16 15:58:45 +0800

    config : cancel eslint displayName setting

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,8 +1,25 @@
-import React, { memo } from "react";
-
+import React, { memo, useEffect, useState } from "react";
+import { getTracks } from "@/apis/home";
 const Home = memo(() => {
-  return <div>home</div>;
+  const [highScore, setHighScore] = useState({});
+
+  useEffect(() => {
+    getTracks().then((res) => {
+      setHighScore(res);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>{highScore.title}</h2>
+      <h4>{highScore.subtitle}</h4>
+      <ul>
+        {highScore?.list?.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 });
 
-// Home.displayName = "Home";
 export default Home;
