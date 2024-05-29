1740c836b926165af56885f01fa64c5a0a19a2ba
qianyi <hgit915@gmail.com>
2024-05-29 18:54:06 +0800

    fix : 1. home / detail 未登入，不用強制導轉至 login 2. 只有登入中才需取得 user 資訊，減少不必要的檢查

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,13 +1,28 @@
 import react, { memo, useEffect } from 'react'
 import { getUserInfoAction } from '@/store/modules/user'
-import { useDispatch } from 'react-redux'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserData } from '@/store/selector/user'
+import { getCookie } from '@/utils/cookies'
 
 const ProtectedRoute = memo(({ children }) => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { isLogin } = useSelector(getUserData)
+  const { pathname } = useLocation()
+
   // 確認登入狀態並取得 userName
   useEffect(() => {
-    dispatch(getUserInfoAction())
-  }, [dispatch])
+    // 登入時取得 User 最新資料
+    if (isLogin || getCookie()) {
+      dispatch(getUserInfoAction())
+    } else {
+      if (pathname.startsWith('/home') || pathname.startsWith('/detail')) return
+      navigate('/login')
+    }
+  }, [dispatch, isLogin, navigate, pathname])
+
   return children
 })
 export default ProtectedRoute
--- src/components/PublicRoute/index.jsx
+++ src/components/PublicRoute/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import react from 'react'
 import { Navigate } from 'react-router-dom'
 import { getCookie } from '@/utils/cookies'
 
 const PublicRoute = ({ children }) => {
   const token = getCookie()
   // 登入情況下，如果輸入 /login、/sign，則會跳轉回首頁
-  if (token) return <Navigate to="/home" replace />
+  if (token) return <Navigate to="/home" />
   return children
 }
 
