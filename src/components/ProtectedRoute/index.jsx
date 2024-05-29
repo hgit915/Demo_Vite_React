5721ef79c4b56f9bd76b82d37b9548e8677cd492
qianyi <hgit915@gmail.com>
2024-05-29 17:30:03 +0800

    fix : home 頁面會重複取 user 資料，調整取一次就好

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import react, { memo, useEffect } from 'react'
 import { getUserInfoAction } from '@/store/modules/user'
 import { useDispatch } from 'react-redux'
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = memo(({ children }) => {
   const dispatch = useDispatch()
   // 確認登入狀態並取得 userName
-  dispatch(getUserInfoAction())
+  useEffect(() => {
+    dispatch(getUserInfoAction())
+  }, [dispatch])
   return children
-}
+})
 export default ProtectedRoute
